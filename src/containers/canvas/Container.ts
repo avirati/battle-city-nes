240959287227b547121e643c512cfd38dd19cf59
Avinash Verma <avinash.verma@oradian.com>
2019-10-13 16:43:35 +0200

    Drawing scene now possible

--- src/containers/canvas/Container.ts
+++ src/containers/canvas/Container.ts
@@ -2,6 +2,7 @@ import { getScreenDimension } from 'helpers';
 
 import {
     BRICK_IMAGE,
+    CELL_SIZE,
     EMPTY_IMAGE,
     GRASS_IMAGE,
     STEEL_IMAGE,
@@ -12,11 +13,13 @@ import {
     IArena,
 } from 'models/Arena';
 import { CellType } from 'models/Cell';
+import { menu } from '../menu/Container';
 
 class Canvas {
     private canvas: HTMLCanvasElement;
     private context: CanvasRenderingContext2D | null;
     private gameData: IArena;
+    private imageMap: Map<string, HTMLImageElement> = new Map();
 
     constructor() {
         this.canvas = document.createElement('canvas');
@@ -25,7 +28,12 @@ class Canvas {
 
         this.setSize();
         this.prepareScene();
-        this.renderScene();
+
+        this.downloadTextures()
+        .then(() => {
+            this.renderScene();
+            this.prepareLevelDesigner();
+        });
     }
 
     public getCanvas = () => this.canvas;
@@ -47,31 +55,18 @@ class Canvas {
     }
 
     private renderScene = () => {
-        const imagePromises = [
-            this.getCellImage(CellType.BRICK),
-            this.getCellImage(CellType.GRASS),
-            this.getCellImage(CellType.STEEL),
-            this.getCellImage(CellType.WATER),
-            this.getCellImage(CellType.EAGLE),
-            this.getCellImage(CellType.EMPTY),
-        ];
-        Promise.all(imagePromises)
-        .then(([brickImage, grassImage, steelImage, waterImage, eagleImage, emptyImage]) => {
-            const imageMap = {
-                [CellType.BRICK]: brickImage,
-                [CellType.GRASS]: grassImage,
-                [CellType.STEEL]: steelImage,
-                [CellType.WATER]: waterImage,
-                [CellType.EAGLE]: eagleImage,
-                [CellType.EMPTY]: emptyImage,
-            };
-            for (let i = 0; i < this.gameData.size; i++) {
-                for (let j = 0; j < this.gameData.size; j++) {
-                    const cell = this.gameData.matrix[i][j];
-                    this.context!.drawImage(imageMap[CellType.EMPTY], cell.position.x, cell.position.y, cell.size, cell.size);
-                }
+        for (let i = 0; i < this.gameData.size; i++) {
+            for (let j = 0; j < this.gameData.size; j++) {
+                const cell = this.gameData.matrix[i][j];
+                this.context!.drawImage(
+                    this.imageMap.get(CellType.EMPTY)!,
+                    cell.position.x,
+                    cell.position.y,
+                    cell.size,
+                    cell.size,
+                );
             }
-        });
+        }
     }
 
     private getCellImage = (cellType: CellType): Promise<HTMLImageElement> => new Promise((resolve, reject) => {
@@ -100,6 +95,51 @@ class Canvas {
         image.onload = () => resolve(image);
         image.onerror = (err) => reject(err);
     })
+
+    private prepareLevelDesigner = () => {
+        this.canvas.addEventListener('click', (event: MouseEvent) => {
+            const x = event.offsetX;
+            const y = event.offsetY;
+
+            const cellColumn = Math.floor(x / CELL_SIZE);
+            const cellRow = Math.floor(y / CELL_SIZE);
+
+            const cell = this.gameData.matrix[cellColumn][cellRow];
+            cell.type = menu.getSelectedCellType();
+            this.context!.drawImage(
+                this.imageMap.get(cell.type)!,
+                cell.position.x,
+                cell.position.y,
+                cell.size,
+                cell.size,
+            );
+        });
+    }
+
+    private downloadTextures = async (): Promise<void> => {
+        const imagePromises = [
+            this.getCellImage(CellType.BRICK),
+            this.getCellImage(CellType.GRASS),
+            this.getCellImage(CellType.STEEL),
+            this.getCellImage(CellType.WATER),
+            this.getCellImage(CellType.EAGLE),
+            this.getCellImage(CellType.EMPTY),
+        ];
+        const [
+            brickImage,
+            grassImage,
+            steelImage,
+            waterImage,
+            eagleImage,
+            emptyImage,
+        ] = await Promise.all(imagePromises);
+        this.imageMap.set(CellType.BRICK, brickImage);
+        this.imageMap.set(CellType.GRASS, grassImage);
+        this.imageMap.set(CellType.STEEL, steelImage);
+        this.imageMap.set(CellType.WATER, waterImage);
+        this.imageMap.set(CellType.EAGLE, eagleImage);
+        this.imageMap.set(CellType.EMPTY, emptyImage);
+    }
 }
 
 export const canvas = new Canvas();
